d706d9661d498864b74167dfc060ac989e28f50c
Finley Lau <finleylau98@gmail.com>
2017-10-29 01:59:26 -0500

    added contact functionality

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, flash, redirect, render_template, request, url_for, jsonify
+from flask import Flask, flash, redirect, render_template, request, url_for, jsonify, flash
 from json import load
 from flask_jsglue import JSGlue
+from forms import ContactForm
+from flask_mail import Message, Mail
+from werkzeug.utils import secure_filename
+import os
 
 # configure app
 app = Flask(__name__,static_url_path="")
 JSGlue(app)
+
+app.secret_key = "s3d6Y0i9tMTuEKmy7FTi"
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "myactimeline@gmail.com"
+app.config["MAIL_PASSWORD"] = "myactimeline12345"
+mail = Mail(app)
 
 # database file
 db_file = "db.json"
@@ -59,10 +72,50 @@ def timeline():
     """Renders timeline page."""
     return render_template("timeline.html")
 
-@app.route("/contribute")
+@app.route("/contribute", methods=["GET","POST"])
 def contribute():
     """Renders contribute page."""
-    return render_template("contribute.html")
+    form = ContactForm()
+
+    if request.method == "POST":
+        if form.validate() == False:
+            flash("Please make sure you've filled out your name, a valid email, the event name, and the event date.")
+            return render_template("contribute.html", form=form)
+        else:
+            IMAGE_FILE_TYPES = set(['jpg', 'jpeg', 'png'])
+            MEDIA_FILE_TYPES = set(['pdf'])
+            msg = Message("MYAC Timeline Contact Email - New Event Request", sender='myactimeline@gmail.com', recipients=["finleylau@mya.org"])
+            msg.body = "A new event request has been made from %s (%s).\n\nEvent Name: %s\nEvent Date: %s\nEvent Description: %s\nEvent Link: %s" % (form.name.data, form.email.data, form.eventname.data, form.date.data, form.description.data, form.link.data)
+
+            if form.image.data != None:
+                image_name = form.image.data.filename
+                if '.' in image_name and image_name.rsplit('.', 1)[1] in IMAGE_FILE_TYPES:
+                    if image_name.rsplit('.',1)[1] == "png":
+                        contenttype = "image/png"
+                    elif image_name.rsplit('.',1)[1] == "jpg" or image_name.rsplit('.',1)[1] == "jpeg":
+                        contenttype = "image/jpeg"
+
+                    filename = secure_filename(image_name)
+                    form.image.data.save(filename)
+                    with app.open_resource(filename) as fp:
+                        msg.attach(filename = filename, content_type = contenttype, data = fp.read())
+                    os.remove(filename)
+
+            if form.image.data != None:
+                media_name = form.media.data.filename
+                if '.' in media_name and media_name.rsplit('.', 1)[1] in MEDIA_FILE_TYPES:
+                    filename = secure_filename(media_name)
+                    form.media.data.save(filename)
+                    with app.open_resource(filename) as fp:
+                        msg.attach(filename, "application/pdf", fp.read())
+                    os.remove(filename)
+
+            mail.send(msg)
+
+            return render_template("contribute.html", success=True)
+
+    elif request.method == "GET":
+        return render_template("contribute.html", form=form)
 
 @app.route("/error")
 def error():
---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import Form
+from wtforms import validators, ValidationError
+from wtforms.fields import TextField, TextAreaField, SubmitField, DateField
+from flask_wtf.file import FileField
+
+class ContactForm(Form):
+  name = TextField("Your Name", [validators.InputRequired("Please enter your name")])
+  email = TextField("Email", [validators.InputRequired("Please enter your email"), validators.Email("Please enter a valid email")])
+  eventname = TextField("Event Name", [validators.InputRequired("Please enter the event name")])
+  date = TextField("Date (mm/dd/yyyy)",[validators.InputRequired("Please enter the event date")])
+  description = TextAreaField("Event Description")
+  image = FileField("Event image - jpg or png")
+  media = FileField("Event media - pdf only")
+  link = TextField("Video link")
+  submit = SubmitField("Submit")
